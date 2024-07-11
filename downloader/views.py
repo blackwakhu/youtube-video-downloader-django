d7314a7858ebd959a229b1bf46ac9a69cf11d771
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-11 09:51:57 +0000

    removed pytube. trying yt-dlp

--- downloader/views.py
+++ downloader/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from pytube import YouTube
 
 
 def index(request):
+    if request.method == "POST":
+        url = request.POST.get("url")
+        yt = YouTube(url)
+        video = yt
+        print(yt)
+        return render(request, 'index.html', {"data": video})
     return render(request, 'index.html')
+
+def download_list(request):
+    return render(request, "index.html")
