d2061efa06a0b777f1849a09cf7c2b1e1f91218c
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-16 06:57:19 +0000

    added functions to the video and audio button

--- downloader/static/js/function.js
+++ downloader/static/js/function.js
@@ -1,2 +1,12 @@
 var video_btn = document.querySelector("#video-btn");
 var audio_btn = document.querySelector("#audio-btn");
+var video_table = document.querySelector(".video-table");
+var audio_table = document.querySelector(".audio-table");
+video_btn.addEventListener('click', function () {
+    video_table.style.display = "block";
+    audio_table.style.display = "none";
+});
+audio_btn.addEventListener('click', function () {
+    video_table.style.display = "none";
+    audio_table.style.display = "block";
+});
--- downloader/static/js/function.ts
+++ downloader/static/js/function.ts
@@ -1,2 +1,14 @@
-let video_btn = document.querySelector("#video-btn");
-let audio_btn = document.querySelector("#audio-btn");
+let video_btn = document.querySelector<HTMLButtonElement>("#video-btn");
+let audio_btn = document.querySelector<HTMLButtonElement>("#audio-btn");
+let video_table = document.querySelector<HTMLTableElement>(".video-table");
+let audio_table = document.querySelector<HTMLTableElement>(".audio-table");
+
+video_btn.addEventListener('click', function(){
+  video_table.style.display = "block";
+  audio_table.style.display = "none";
+})
+
+audio_btn.addEventListener('click', function(){
+  video_table.style.display = "none";
+  audio_table.style.display = "block";
+})
