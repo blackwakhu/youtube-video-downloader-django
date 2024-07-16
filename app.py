8cb58c7e4c54bec5952ebf309828a0478cf94eaf
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-16 06:43:12 +0000

    added typescript

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     print("Available formats:")
     for format in info.get('formats', []):
         if format['ext'] == "mp4":
-            if 'height' not in format.keys():
-                format['height'] = "None"
-            if  'filesize' not in format.keys():
-                format['filesize'] = 'None'
-            print(f"  - {format['format_id']} - {format['ext']} {format['height']}p {format['filesize']} {format['format']}")
+            if 'format_note' not in format.keys():
+                format['format_note'] = "video"
+            print(f"{format['format_note']}")
+            # if 'height' not in format.keys():
+            #     format['height'] = "None"
+            # if  'filesize' not in format.keys():
+            #     format['filesize'] = 'None'
+            # print(f"  - {format['format_id']} - {format['ext']} {format['height']}p {format['filesize']} {format['format']}")
             # print(f"{format.keys()}")
+            
         # else:
             # print(f"  - {format['format_id']} - {format['ext']}")
             
